b753957afe3329b46525de9fdaccc8f66a86816b
Daniela2319 <frankvt04@gmail.com>
2025-07-26 13:21:16 -0300

    database connection created

--- trilha-net-api.Sol/trilha-net-api.Data/OrganizerContext.cs
+++ trilha-net-api.Sol/trilha-net-api.Data/OrganizerContext.cs
@@ -1,7 +1,17 @@
-﻿namespace trilha_net_api.Data
-{
-    public class OrganizerContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using trilha_net_api.Models;
+using trilha_net_api.Services;
 
+namespace trilha_net_api.Data
+{
+    public class OrganizerContext : DbContext
+    {
+        public OrganizerContext(DbContextOptions<OrganizerContext> options) : base(options)
+        {
+        }
+        public DbSet<TaskA> Tasks { get; set; }
     }
+       
+
+        
 }
