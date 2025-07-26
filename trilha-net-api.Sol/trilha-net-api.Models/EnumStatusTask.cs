cc884ab04872b3c06437db98c0e77140648193dd
Daniela2319 <frankvt04@gmail.com>
2025-07-26 10:03:18 -0300

    the TaskA class was created in the model project

---
+++ trilha-net-api.Sol/trilha-net-api.Models/EnumStatusTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trilha_net_api.Models
+{
+    public enum EnumStatusTask
+    {
+        Pending,
+        finished
+    }
+}
